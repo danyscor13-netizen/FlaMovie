4a707a80c96b4a23bdb5c124dc255fbda800e688
danyscor13-netizen <danyscor13@gmail.com>
2026-07-01 21:58:58 +0000

    Fondamenta 1

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask, render_template
+
+app = Flask(__app__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False,
+    )
